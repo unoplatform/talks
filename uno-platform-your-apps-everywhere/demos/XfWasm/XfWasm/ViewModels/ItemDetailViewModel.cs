3a06cda66f7bd757413cebe6e5d50547d8de19ca
Martin Zikmund <martin@zikmund.dev>
2020-01-13 18:20:01 +0100

    Added Your apps, everywhere talk (#19)

---
+++ uno-platform-your-apps-everywhere/demos/XfWasm/XfWasm/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XfWasm.Models;
+
+namespace XfWasm.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
